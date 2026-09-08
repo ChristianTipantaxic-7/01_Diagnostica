7d1b07200a4f3e2963925352888f46f9e516c988
ChristianTipantaxic-7 <cristipantaxic@gmail.com>
2026-09-07 21:06:58 -0500

    feat(bloque_2): Realice el ejercicio del bloque 2 del potencimetro y lectura de voltaje

--- bloque_2/src/bloque_2.ino
+++ bloque_2/src/bloque_2.ino
@@ -1,21 +1,29 @@
 const int pinPot = 34;
 
 void setup() {
+
   Serial.begin(115200);
+
 }
 
 void loop() {
-  // TODO: Leer el valor del potenciómetro usando analogRead
-  int lecturaADC = 0; // Cambia esto
-  
-  // TODO: Convertir la lectura de 12 bits (0-4095) a Voltaje (0-3.3V)
-  float voltaje = 0.0; // Cambia esto aplicando la regla de tres
-  
+
+  // CHECK: Leer el valor del potenciómetro usando analogRead
+  int lecturaADC = analogRead(pinPot);
+
+  // CHECK: Convertir la lectura de 12 bits (0-4095) a Voltaje (0-3.3V)
+  float voltaje = (lecturaADC * 3.3) / 4095.0;
+
   Serial.print("ADC: ");
+
   Serial.print(lecturaADC);
+
   Serial.print(" | Voltaje: ");
+
   Serial.print(voltaje, 2);
+
   Serial.println("V");
-  
+
   delay(500);
+
 }
